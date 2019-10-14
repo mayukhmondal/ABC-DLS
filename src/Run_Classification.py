ccd9ba6248e8f40f1c82205ab7334bc64a9a8ceb
Mayukh Mondal <mondal.mayukh@gmail.com>
2019-10-14 11:46:36 +0300

    inspect run

--- src/Run_Classification.py
+++ src/Run_Classification.py
@@ -7,31 +7,34 @@ import argparse
 
 from Classes import ABC
 
-###to stop future warning every time to print out
+# to stop future warning every time to print out
 
-##input argument is done
+# input argument is done
 parser = argparse.ArgumentParser(description='This is run classification between models using any summary statistics.\n'
                                              'The info file should look like this\n'
                                              'file1.csv.gz    10\n'
                                              'file2.csv.gz    14\n', formatter_class=argparse.RawTextHelpFormatter)
 subparsers = parser.add_subparsers(help='sub-commands help')
 
-sp = subparsers.add_parser('All', help='The whole run of the NN for paramter estimation from first to last')
+sp = subparsers.add_parser('All', help='The whole run of the NN for parameter estimation from first to last')
 sp.set_defaults(cmd='All')
 sp.add_argument('info',
-                help='the path for the info file. whose firstcolumn will be file path (.csv or .csv.gz) and tab delimited column with and the number of paramter in that file. ex. <file1.csv.gz> <n>')
+                help='the path for the info file. whose first column will be file path (.csv or .csv.gz) and tab '
+                     'delimited column with and the number of parameter in that file. ex. <file1.csv.gz> <n>')
 sp.add_argument('--test_size',
                 help='test size for r abc. everything else will be used for training purpose. default is 10 thousands',
                 default=10000, type=int)
 sp.add_argument('--chunksize',
-                help='If two big for the memroy use chunk size. relatively slow but no problem with ram',
+                help='If two big for the memory use chunk size. relatively slow but no problem with ram',
                 type=float)
 sp.add_argument('--scale', help="To scale the data ", action="store_true")
 sp.add_argument('--ssfile', help="The summary statistics file from real data", required=True)
 sp.add_argument('--demography',
-                help='The demography.py file full path. If this is given it will assume it has better function cater to your own demography. The def ANNModelCheck should be inside')
+                help='The demography.py file full path. If this is given it will assume it has better function cater to'
+                     ' your own demography. The def ANNModelCheck should be inside')
 sp.add_argument('--method',
-                help='Method used for R abc classification. can be  "rejection", "mnlogistic", "neuralnet". default is rejection" ',
+                help='Method used for R abc classification. can be  "rejection", "mnlogistic", "neuralnet". default is'
+                     ' rejection" ',
                 default='rejection', choices=["rejection", "mnlogistic", "neuralnet"])
 sp.add_argument('--tolerance', help='tolerance limit for r abc. default is .005 ', default=.005, type=float)
 sp.add_argument('--csvout',
@@ -41,38 +44,44 @@ sp.add_argument('--csvout',
 sp = subparsers.add_parser('Pre_train', help='To prepare the data for training ANN.')
 sp.set_defaults(cmd='Pre_train')
 sp.add_argument('info',
-                help='the path for the info file. whose firstcolumn will be file path (.csv or .csv.gz) and tab delimited column with and the number of paramter in that file. ex. <file1.csv.gz> <n>')
+                help='the path for the info file. whose first column will be file path (.csv or .csv.gz) and tab '
+                     'delimited column with and the number of parameter in that file. ex. <file1.csv.gz> <n>')
 sp.add_argument('--chunksize',
-                help='If two big for the memroy use chunk size. relatively slow but no problem with ram. In this case, chunksize is mandatory. default value 10000',
+                help='If two big for the memory use chunk size. relatively slow but no problem with ram. In this case,'
+                     ' chunksize is mandatory. default value 10000',
                 type=float, default=10000)
 sp.add_argument('--scale', help="To scale the data ", action="store_true")
 
-sp = subparsers.add_parser('Train', help='The trainging part of the ANN. Should be done after Pre_train part')
+sp = subparsers.add_parser('Train', help='The training part of the ANN. Should be done after Pre_train part')
 sp.set_defaults(cmd='Train')
 sp.add_argument('--demography',
-                help='The demography.py file full path. If this is given it will assume it has better function cater to your own demography. The def ANNModelCheck should be inside')
+                help='The demography.py file full path. If this is given it will assume it has better function cater to'
+                     ' your own demography. The def ANNModelCheck should be inside')
 sp.add_argument('--test_size',
                 help='test size for r abc. everything else will be used for training purpose. default is 10 thousands',
                 default=10000, type=int)
 sp = subparsers.add_parser('CV',
-                           help='After the training only to get the result of cross validation test. Good for unavailable real data')
+                           help='After the training only to get the result of cross validation test. Good for '
+                                'unavailable real data')
 sp.set_defaults(cmd='CV')
 sp.add_argument('--test_size',
                 help='test size for r abc. everything else will be used for training purpose. default is 10 thousands',
                 default=10000, type=int)
 sp.add_argument('--method',
-                help='Method used for R abc classification. can be  "rejection", "mnlogistic", "neuralnet". default is rejection" ',
+                help='Method used for R abc classification. can be  "rejection", "mnlogistic", "neuralnet". default is'
+                     ' rejection" ',
                 default='rejection', choices=["rejection", "mnlogistic", "neuralnet"])
 sp.add_argument('--tolerance', help='tolerance limit for r abc. default is .005 ', default=.005, type=float)
 
-sp = subparsers.add_parser('After_train', help='This is to run the ABC analysis after the traingin part is done')
+sp = subparsers.add_parser('After_train', help='This is to run the ABC analysis after the training part is done')
 sp.set_defaults(cmd='After_train')
 sp.add_argument('--ssfile', help="The summary statistics file from real data", required=True)
 sp.add_argument('--test_size',
                 help='test size for r abc. everything else will be used for training purpose. default is 10 thousands',
                 default=10000, type=int)
 sp.add_argument('--method',
-                help='Method used for R abc classification. can be  "rejection", "mnlogistic", "neuralnet". default is rejection" ',
+                help='Method used for R abc classification. can be  "rejection", "mnlogistic", "neuralnet". default is'
+                     ' rejection" ',
                 default='rejection', choices=["rejection", "mnlogistic", "neuralnet"])
 sp.add_argument('--tolerance', help='tolerance limit for r abc. default is .005 ', default=.005, type=float)
 sp.add_argument('--csvout',
@@ -80,7 +89,7 @@ sp.add_argument('--csvout',
                 action="store_true")
 args = parser.parse_args()
 
-###running
+# running
 if args.cmd == 'All':
     if args.chunksize:
         args.chunksize = int(args.chunksize)
