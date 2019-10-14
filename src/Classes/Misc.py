2b47cdf17b90a4fc4892e9caa2773030ffb8dfb2
Mayukh Mondal <mondal.mayukh@gmail.com>
2019-10-14 11:57:07 +0300

    inspect run

--- src/Classes/Misc.py
+++ src/Classes/Misc.py
@@ -3,11 +3,14 @@
 This file is for miscellaneous def
 """
 import os
-##type hint for readability
-from typing import List, Dict, Tuple, Optional, Callable, Union
+# type hint for readability
+from typing import Tuple, Optional, Callable
 
-####R packaage check
-def importr_tryhard(packname:str):
+
+# R package check
+
+
+def importr_tryhard(packname: str):
     """
     It will check if the package is installed under rpy2. if not it will automatically install it from the fist online
     server
@@ -26,8 +29,11 @@ def importr_tryhard(packname:str):
         rpack = importr(packname)
     return rpack
 
-######reading filesssss
-def reading_bylines_small(filepath:str)-> list:
+
+# reading files
+
+
+def reading_bylines_small(filepath: str) -> list:
     """
     Will return a list where every element is a line. Very basic and can only be done on small files
 
@@ -39,7 +45,8 @@ def reading_bylines_small(filepath:str)-> list:
     lines = [line.strip("\n") for line in lines]
     return lines
 
-def getting_line_count(file:str)-> int:
+
+def getting_line_count(file: str) -> int:
     """
     To get the the number of lines present in the file
 
@@ -47,16 +54,19 @@ def getting_line_count(file:str)-> int:
     :return: will return the line count in total
     """
     import gzip
-    if file[-3:]=='.gz':
+    if file[-3:] == '.gz':
         with gzip.open(file, 'rb') as f:
-            count=sum(1 for _ in f)
+            count = sum(1 for _ in f)
     else:
-        with open(file,'r') as f:
-            count=sum(1 for _ in f)
+        with open(file, 'r') as f:
+            count = sum(1 for _ in f)
     return count
 
-##file name manipulations
-def gettingfilename(filepath:str)-> str:
+
+# file name manipulations
+
+
+def gettingfilename(filepath: str) -> str:
     """
     It wil give back the name of the file from the filepath
 
@@ -70,7 +80,7 @@ def gettingfilename(filepath:str)-> str:
         return filepath
 
 
-def gettingextension(filepath:str)->str:
+def gettingextension(filepath: str) -> str:
     """
     As the name suggest from the filepath it will give the file extension. Remember if the filename do not have dot (.)
     it will give back the whole filename
@@ -85,7 +95,9 @@ def gettingextension(filepath:str)->str:
         return splitfilename[len(splitfilename) - 1]
     else:
         return ""
-def filenamewithoutextension(filepath:str)-> str:
+
+
+def filenamewithoutextension(filepath: str) -> str:
     """
     As the name suggest it will remove the file extension from the full filename. Additionally if its in a path it will
     remove the path as well. If it has multiple dot will only remove the last one (i.e. bla.vcf.gz will give bla.vcf)
@@ -99,7 +111,9 @@ def filenamewithoutextension(filepath:str)-> str:
         return filename[:-(len(extension) + 1)]
     else:
         return filename
-def filenamewithoutextension_checking_zipped(filepath:str)->str:
+
+
+def filenamewithoutextension_checking_zipped(filepath: str) -> str:
     """
     As the name suggest it is like filename without extension but it will check if the file is zipped (end with gz) and
     then run it twice to remove double extension. name.vcf.gz/name.vcf will give name
@@ -112,8 +126,11 @@ def filenamewithoutextension_checking_zipped(filepath:str)->str:
         filename = filenamewithoutextension(filename)
     return filename
 
-##files checker
-def file_existence_checker(names:list, files:list) -> Tuple[str,list]:
+
+# files checker
+
+
+def file_existence_checker(names: list, files: list) -> Tuple[str, list]:
     """
     As most of the clasess needs different type of check of existence of the file. It will check if the file exist and
     then return appropriate error message to print.
@@ -138,8 +155,11 @@ def file_existence_checker(names:list, files:list) -> Tuple[str,list]:
             absence.append(file)
     return joinginglistbyspecificstring(printer, "\n"), absence
 
-#######python string commands
-def joinginglistbyspecificstring(listinput:list, string:str=" ")->str:
+
+# python string commands
+
+
+def joinginglistbyspecificstring(listinput: list, string: str = " ") -> str:
     """
     It join a list with the given string
 
@@ -147,12 +167,15 @@ def joinginglistbyspecificstring(listinput:list, string:str=" ")->str:
     :param string: the string with which it has to join. by default is space
     :return: the joined string
     """
-    listinput = [x for x in listinput if x is not None]  ##to remove none
+    listinput = [x for x in listinput if x is not None]  # to remove none
     listinput = list(map(str, listinput))
     return string.join(listinput)
 
-##del files
-def removefilescommands(files:list)->list:
+
+# del files
+
+
+def removefilescommands(files: list) -> list:
     """
     Giving a list of files it will give back the commands necessary for deleting such files.
 
@@ -162,25 +185,29 @@ def removefilescommands(files:list)->list:
 
     return ['rm -f ' + file for file in files]
 
-def removefiles(files:list,printing:bool=True)-> None:
+
+def removefiles(files: list, printing: bool = True) -> None:
     """
     Its the wrapper of removefiles commands. if you want ot remove the files right now. also print it is removing.
     good to know which files are getting removed always. It will print remove even if the file do not exist in the first
     place
 
     :param files: the list of files
-    :param print: to print if removing the file. bool. default is true. If false noting will be printed just deleted
+    :param printing: to print if removing the file. bool. default is true. If false noting will be printed just deleted
     :return: will not return anything just remove and print
     """
-    commands=removefilescommands(files)
+    commands = removefilescommands(files)
     if printing:
         for file in files:
-            print ("Removing if exist:",file)
+            print("Removing if exist:", file)
     [os.system(command) for command in commands]
     return None
 
-####hdf5 commands
-def numpy2hdf5(x,outfile:str,dataset:str='mydata')-> None:
+
+# hdf5 commands
+
+
+def numpy2hdf5(x, outfile: str, dataset: str = 'mydata') -> None:
     """
     This will save the numpy to hdf5 format.
 
@@ -191,13 +218,16 @@ def numpy2hdf5(x,outfile:str,dataset:str='mydata')-> None:
     """
     import h5py
     import numpy
-    x=numpy.array(x)
+    x = numpy.array(x)
     f = h5py.File(outfile, 'w')
-    h5f = f.create_dataset(dataset, data=x)
+    f.create_dataset(dataset, data=x)
     f.close()
 
-####importing functions
-def loading_def_4m_file(filepath:str,defname:str)-> Optional[Callable]:
+
+# importing functions
+
+
+def loading_def_4m_file(filepath: str, defname: str) -> Optional[Callable]:
     """
     loading defintion from a file. some of the mar deprecate. will check later.
 
@@ -205,9 +235,9 @@ def loading_def_4m_file(filepath:str,defname:str)-> Optional[Callable]:
     :param defname: the name of the definition
     :return: will return the func if exist in the file. if not will return None
     """
-    import importlib,imp
+    import importlib, imp
     if not os.path.isfile(filepath):
-        print ('The file count not be found to load. Please check:',filepath)
+        print('The file count not be found to load. Please check:', filepath)
     modname = filenamewithoutextension(filepath)
     mod = imp.load_source(modname, filepath)
     if defname in dir(mod):
@@ -217,8 +247,12 @@ def loading_def_4m_file(filepath:str,defname:str)-> Optional[Callable]:
         return getattr(module, defname)
     else:
         return None
-##shell script commands
-def creatingfolders(specificfolder:str)-> None:
+
+
+# shell script commands
+
+
+def creatingfolders(specificfolder: str) -> None:
     """
     As the name suggest it will create a folder if the folder do not exist. As simple as that
 
