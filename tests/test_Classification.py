a1c10cf825ff56fde7ce0da2c6c7f7467ebefd77
mayukhmondal <mondal.mayukh@gmail.com>
2019-10-18 21:59:35 +0300

    pytest check

--- tests/test_Classification.py
+++ tests/test_Classification.py
@@ -11,22 +11,26 @@ from src.Classes import ABC
 from collections import Counter
 from keras.utils import HDF5Matrix
 import pandas
+
+
 def test_Classification_Pre_train():
     info='Model.info'
     test_size=1
     chunksize=5
-    scale=False
+    scale=True
+
+    #
     ABC.ABC_TFK_Classification_PreTrain(info=info, test_size=test_size, chunksize=chunksize,scale=scale)
 
     # file existen check
-    files=['x.h5','y.h5','y_cat_dict.txt']
+    files=['x.h5','y.h5','y_cat_dict.txt','scale_x.sav']
     not_exist=[file for file in files if not Path(file).exists()]
     assert not not_exist, f'{not_exist} file was not created by ABC_TFK_Classification_PreTrain'
 
     # model name check
     files, paramnumbers, names = ABC.ABC_TFK_Classification_PreTrain.read_info('Model.info')
     y_cat_names = list(eval(open('y_cat_dict.txt', 'r').read()).values())
-    assert Counter(y_cat_names) == Counter(names), 'Model.info model names do not match with y_cat_dict.txt'
+    assert Counter(y_cat_names) == Counter(['OOA', 'OOA_B', 'OOA_M']), 'Model.info model names do not match with y_cat_dict.txt'
 
     # h5 file checks
 
@@ -36,9 +40,8 @@ def test_Classification_Pre_train():
     assert xshape[0]==yshape[0], 'Row numbers between x.h5 and/or y.h5 file do not match'
     assert xshape[0]==15, 'Row numbers of h5 files do not match with expected number of rows'
     # ycol check
-    assert yshape[1]==len(names), 'Number of columns in y.h5 do not match with the number of names present in ' \
+    assert yshape[1]==3, 'Number of columns in y.h5 do not match with the number of names present in ' \
                                   'Model.info'
     #ncol check
-    expectedcol=pandas.read_csv(files[0]).shape[1]-paramnumbers[0]
-    assert xshape[1]==expectedcol, 'Column number of x.h5 do not match with expected'
+    assert xshape[1]==1331, 'Column number of x.h5 do not match with expected'
     # checking y.h5 file
