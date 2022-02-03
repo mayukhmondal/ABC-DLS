559f94b9b6ea72064d775f8b0b5327aaed7618b3
mayukh <mondal.mayukh@gmail.com>
2022-02-03 12:21:33 +0200

    updating to print the current state of Oldrange in the terminal

--- src/SFS/recursive.sh
+++ src/SFS/recursive.sh
@@ -4,6 +4,7 @@ touch Narrowed.csv All.csv
 cp Startrange.csv Oldrange.csv
 while [ "$(echo "$imp < 0.95"| bc -l)"  -eq 1 ]
 do
+	cat <(echo "Params,Lower,Upper,imp")  Oldrange.csv | tr "," "\t"
 	snakemake -q --jobs 6
 	imp=$(cut -f4  -d ","  Newrange.csv |sort -n | head -n 1)
 	mv Newrange.csv Oldrange.csv
