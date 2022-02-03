b2b6bcfc243e677d55610ecebb525088b0b1291b
mayukh <mondal.mayukh@gmail.com>
2022-02-03 16:53:22 +0200

    to make it POSIX compatible version

--- src/SFS/recursive.sh
+++ src/SFS/recursive.sh
@@ -4,7 +4,7 @@ touch Narrowed.csv All.csv #To reuse already ran simulations. All.csv has all th
 cp Startrange.csv Oldrange.csv #hardrange or Starting Range as you like to call it. move it the Oldrange to initialise the loop
 while [ "$(echo "$imp < 0.95"| bc -l)"  -eq 1 ] #if no improvement is less than 95 stop the loop. we reached convergence
 do
-	cat <(echo "Params,Lower,Upper,imp")  Oldrange.csv | tr "," "\t"  #this will print the current state in the terminal
+	echo "Params,Lower,Upper,imp" | cat - Oldrange.csv | tr "," "\t"  #this will print the current state in the terminal
 	snakemake -q --jobs 6    #snakemake running quietly
 	imp=$(cut -f4  -d ","  Newrange.csv |sort -n | head -n 1) #calculating the improvement we got for one run
 	mv Newrange.csv Oldrange.csv # to start it again for another round
